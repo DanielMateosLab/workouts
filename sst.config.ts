c1489710cbcae8fa8fc24e04c76ad539841da115
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-03-28 22:43:36 +0100

    Add cognito user pool

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,19 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+import * as aws from "@pulumi/aws";
+
+const userPool = new aws.cognito.UserPool("WorkoutsUserPool", {
+  accountRecoverySetting: {
+    recoveryMechanisms: [{ name: "verified_email", priority: 1 }],
+  },
+  autoVerifiedAttributes: ["email"],
+  usernameAttributes: ["email"],
+});
+const userPoolClient = new aws.cognito.UserPoolClient(
+  "WorkoutsUserPoolClient",
+  { userPoolId: userPool.id },
+);
+
 export default $config({
   app(input) {
     return {
@@ -14,6 +28,11 @@ export default $config({
         domainName: "workouts.danielmatlab.com",
         redirects: ["www.workouts.danielmatlab.com"],
         hostedZone: "danielmatlab.com",
+      },
+      environment: {
+        NEXT_PUBLIC_USER_POOL_ID: userPool.id,
+        NEXT_PUBLIC_USER_POOL_CLIENT_ID: userPoolClient.id,
+        NEXT_PUBLIC_REGION: aws.config.requireRegion(),
       },
     });
   },
